26d3efb51e3f3b4e1d607e350039b1037522d5ee
WuHaoTian12 <101766788+WuHaoTian12@users.noreply.github.com>
2022-05-22 09:36:09 +0800

    Rewrite the loading method of the void ShowFinalMap()

--- model.c
+++ model.c
@@ -1,24 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <string.h>
+#include <SDL2/SDL.h>
 #include "worldControl.h"
 #include "model.h"
 #include "game.h"
 
 
 
-
+//this model allow users to enter a step, 
+//then the world will iteration to this step
 void SpecificStep() {
+	  // initialize the global variables
 	ifContinue = 0;
 	iterationNum = 0;
 	char aim[1024];
+	  //load world and recive entering
 	FILE* file = fopen("world.txt", "r");
 	LoadMap(file);
 	fclose(file);
 	file = NULL;
 	printf("Please enter the wanted number of iterations: ");
 	scanf("%[^\n]s", aim);
+	
 	if (IfNumValid(aim)) {
 		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 1.\033[0m\n");
 		return;
@@ -26,21 +30,29 @@ void SpecificStep() {
 	else {
 		aimIterationNum = StringToInt(aim);
 	}
+	// draw thw window and deal with events
 	InitialSDL();
 	SDL_Event event;
 	while (iterationNum <= aimIterationNum) {
 		PresentMap();
-		SDL_Delay(1000);
+		SDL_Delay(delay);
 		while (SDL_PollEvent(&event)) {
+			    // clicking event: if click stop button, stop, if click run button, run 
 			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				if (event.button.x > 150 && event.button.x < 350 && event.button.y>865 && event.button.y < 945) {
+				if (event.button.x > 66 && event.button.x < 266 && event.button.y>670 && event.button.y < 850) {
 					ifContinue = 0;
 				}
-				else if (event.button.x > 450 && event.button.x < 650 && event.button.y>865 && event.button.y < 945) {
+				else if (event.button.x > 332 && event.button.x < 532 && event.button.y>670 && event.button.y < 850) {
 					ifContinue = 1;
 				}
 			}
+						// if click quit, free array and store
 			else if (event.type == SDL_QUIT) {
+				file = fopen("final.txt", "w");
+				StoreMap(file);
+				fclose(file);
+				file = NULL;
+				FreeMap();
 				return;
 			}
 		}
@@ -49,32 +61,46 @@ void SpecificStep() {
 			iterationNum++;
 		}
 	}
+	// end free and store
+	file = fopen("final.txt", "w");
+	StoreMap(file);
+	fclose(file);
+	file = NULL;
+	FreeMap();
 }
 
 
+//in this model the world will continue to iterate until the end
 void AutomaticStep() {
-	int end = 0;
+	int end = 0; // judge if the world is final
 	ifContinue = 0;
 	iterationNum = 0;
+	// load world and initialize SDL
 	FILE* file = fopen("world.txt", "r");
 	LoadMap(file);
 	fclose(file);
 	file = NULL;
 	InitialSDL();
+	// draw thw window and deal with events
 	SDL_Event event;
 	while (!end) {
 		PresentMap();
-		SDL_Delay(1000);	
+		SDL_Delay(delay);	
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				if (event.button.x > 150 && event.button.x < 350 && event.button.y>865 && event.button.y < 945) {
+				if (event.button.x > 66 && event.button.x < 266 && event.button.y>670 && event.button.y < 750) {
 					ifContinue = 0;
 				}
-				else if (event.button.x > 450 && event.button.x < 650 && event.button.y>865 && event.button.y < 945) {
+				else if (event.button.x > 332 && event.button.x < 532 && event.button.y>670 && event.button.y < 750) {
 					ifContinue = 1;
 				}
 			}
 			else if (event.type == SDL_QUIT) {
+				file = fopen("final.txt", "w");
+				StoreMap(file);
+				fclose(file);
+				file = NULL;
+				FreeMap();
 				return;
 			}
 		}
@@ -83,19 +109,29 @@ void AutomaticStep() {
 			iterationNum++;
 		}	
 	}
+	file = fopen("final.txt", "w");
+	StoreMap(file);
+	fclose(file);
+	file = NULL;
+	FreeMap();
 }
 
 
-
+//this model allow users to customize the world
+//users need to enter the number of rows, columns, and time of delay
+//users can initialize the world by clicking to define the state of cell
 void selfDefinedMap() {
-	int ifready = 0;
+	int ifready = 0; // judge if users have finished initializing
 	char entering[1024];
+	ifContinue = 0;
+	iterationNum = 0;
+	//recive row, column and delay
 	memset(entering, '\0', 1024);
 	printf("Please enter the number of rows: ");
 	scanf("%[^\n]s", entering);
 	getchar();
 	if (IfNumValid(entering)) {
-		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 1.\033[0m\n");
+		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 0.\033[0m\n");
 		return;
 	}
 	else {
@@ -106,12 +142,24 @@ void selfDefinedMap() {
 	scanf("%[^\n]s", entering);
 	getchar();
 	if (IfNumValid(entering)) {
-		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 1.\033[0m\n");
+		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 0.\033[0m\n");
 		return;
 	}
 	else {
 		colSize = StringToInt(entering);
 	}
+	memset(entering, '\0', 1024);
+	printf("Please enter the time of delay: ");
+	scanf("%[^\n]s", entering);
+	getchar();
+	if (IfNumValid(entering)) {
+		printf("\n\033[47;31mInvalid entering. Expected entering: a number greater than 0.\033[0m\n");
+		return;
+	}
+	else {
+		delay = StringToInt(entering);
+	}
+	//initialize SDL and the world
 	InitialSDL();
 	newMap = CreateMap();
 	beforeMap = CreateMap();
@@ -120,16 +168,24 @@ void selfDefinedMap() {
 		PresentMap();
 		SDL_Delay(250);
 		while (SDL_PollEvent(&event)) {
+			//click event: click in the range of a cell, it will change the state, alive to dead, dead to alive
 			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				if (event.button.x >= 0 && event.button.x <= 800 && event.button.y >= 60 && event.button.y <= 860) {
-					AlterMap((event.button.y - 60) / (800 / rowSize), event.button.x / (800 / colSize));
+				if (event.button.x >= leftSize && event.button.x <= leftSize + edgeSize * colSize 
+					&& event.button.y >= topSize && event.button.y <= topSize + edgeSize * rowSize) {
+					AlterMap((event.button.y - topSize) / edgeSize, (event.button.x - leftSize) / edgeSize);
 				}
-				else if (event.button.x > 450 && event.button.x < 650 && event.button.y>865 && event.button.y < 945) {
+				//click run button, finish innitializing 
+				else if (event.button.x > 332 && event.button.x < 532 && event.button.y>670 && event.button.y < 750) {
 					ifContinue = 1;
 					ifready = 1;
 				}
 			}
 			else if (event.type == SDL_QUIT) {
+				FILE* file = fopen("final.txt", "w");
+				StoreMap(file);
+				fclose(file);
+				file = NULL;
+				FreeMap();
 				return;
 			}
 		}
@@ -137,19 +193,25 @@ void selfDefinedMap() {
 	}
 	iterationNum = 0;
 	int end = 0;
+	//continue iterating until the end
 	while (!end) {
 		PresentMap();
-		SDL_Delay(1000);
+		SDL_Delay(delay);
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				if (event.button.x > 150 && event.button.x < 350 && event.button.y>865 && event.button.y < 945) {
+				if (event.button.x > 66 && event.button.x < 266 && event.button.y>670 && event.button.y < 750) {
 					ifContinue = 0;
 				}
-				else if (event.button.x > 450 && event.button.x < 650 && event.button.y>865 && event.button.y < 945) {
+				else if (event.button.x > 332 && event.button.x < 532 && event.button.y>670 && event.button.y < 750) {
 					ifContinue = 1;
 				}
 			}
 			else if (event.type == SDL_QUIT) {
+				FILE* file = fopen("final.txt", "w");
+				StoreMap(file);
+				fclose(file);
+				file = NULL;
+				FreeMap();
 				return;
 			}
 		}
@@ -158,40 +220,41 @@ void selfDefinedMap() {
 			iterationNum++;
 		}
 	}
-
+	FILE* file = fopen("final.txt", "w");
+	StoreMap(file);
+	fclose(file);
+	file = NULL;
+	FreeMap();
 }
 
 
-
-
-
-
-
-
-
-int LoadFinalMap(FILE* file) {
-	if (file == NULL) {
-		return 0;
-	}
-	char ch1[1024];
-	char row[1024];
-	char col[1024];
-	memset(ch1, '\0', 1024);
-	memset(row, '\0', 1024);
-	memset(col, '\0', 1024);
-	newMap = CreateMap();
-	for (int i = 0; i < rowSize; i++) {
-		memset(ch1, '\0', 1024);
-		fgets(ch1, 1024, file);
-		for (int j = 0; j < colSize; j++) {
-			newMap[i][j] = (int)(ch1[j]) - 48;
+//show the final world stored in final.txt
+void ShowFinalMap() {
+	FILE* file = fopen("final.txt", "r");
+	LoadMap(file);
+	fclose(file);
+	file = NULL;
+	InitialSDL();
+	int i, j;
+	for (i = 0; i < rowSize; i++) {
+		for (j = 0; j < colSize; j++) {
+			if (newMap[i][j] == 1) {
+				Apply(i, j, renderer, aliveCellTexture);
+			}
+			else if (newMap[i][j] == 0) {
+				Apply(i, j, renderer, deadCellTexture);
+			}
 		}
 	}
-
+	SDL_RenderPresent(renderer);
+	SDL_Delay(2000);
+	FreeMap();
 }
 
 
-
+//judge if the entering number string is valid:
+//doesn't begin with space and 0, doesn't end with space 
+//and no characters other than numbers 
 int IfNumValid(char* num) {
 	return (int)num[0] == 48 || num[0] == ' ' || num[strlen(num) - 1] == ' ' ||
 		strspn(num, "0123456789") != strlen(num) ||  strlen(num) <= 0;
--- model.h
+++ model.h
@@ -1,10 +1,24 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
-
-
-void AutomaticStep();
-int IfNumValid(char* num);
+//this model allow users to enter a step, 
+//then the world will iteration to this step
 void SpecificStep();
+
+//in this model the world will continue to iterate until the end
+void AutomaticStep();
+
+//this model allow users to customize the world
+//users need to enter the number of rows, columns, and time of delay
+//users can initialize the world by clicking to define the state of cell
 void selfDefinedMap();
+
+//show the final world stored in final.txt
+void ShowFinalMap();
+
+//judge if the entering number string is valid:
+//doesn't begin with space and 0, doesn't end with space 
+//and no characters other than numbers 
+int IfNumValid(char* num);
+
 #endif
